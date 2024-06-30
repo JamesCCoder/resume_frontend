e63d754758cf9afd722f40c7f3e6d5df599eb9d5
James <justinwpro@gmail.com>
2024-06-29 19:48:31 -0500

    update

--- src/MainPage/components/ColorChangeButton.tsx
+++ src/MainPage/components/ColorChangeButton.tsx
@@ -21,10 +21,10 @@ const ColorChangeButton: React.FC = () => {
     const currentDecoColor = getComputedStyle(root).getPropertyValue('--global-decocolor').trim();
     const currentDecoColorSecond = getComputedStyle(root).getPropertyValue('--global-decocolor-second').trim();
   
-    const newBackColor = currentBackColor === 'rgb(30, 29, 38)' ? 'rgb(240, 255, 240)' : 'rgb(30, 29, 38)';
-    const newFontColor = currentFontColor === 'white' ? 'rgb(30, 29, 38)' : 'white';
-    const newDecoColor = currentDecoColor === 'rgb(0, 255, 165)' ? 'rgb(46, 139, 87)' :'rgb(0, 255, 165)';
-    const newDecoColorSecond = currentDecoColorSecond === 'rgb(45,44,50)' ? 'rgb(64, 220, 165)' :'rgb(45,44,50)';
+    const newBackColor = currentBackColor === 'rgb(30, 29, 38)' ?  'rgb(30, 29, 38)': 'rgb(240, 255, 240)';
+    const newFontColor = currentFontColor === 'white' ? 'white': 'rgb(30, 29, 38)';
+    const newDecoColor = currentDecoColor === 'rgb(0, 255, 165)' ? 'rgb(0, 255, 165)':'rgb(46, 139, 87)';
+    const newDecoColorSecond = currentDecoColorSecond === 'rgb(45,44,50)' ? 'rgb(45,44,50)':'rgb(64, 220, 165)';
 
     root.style.setProperty('--global-backcolor', newBackColor);
     root.style.setProperty('--global-fontcolor', newFontColor);
